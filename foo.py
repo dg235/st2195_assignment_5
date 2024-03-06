0807543b764c62801c364b424f1105756f6a9d7c
Dorothea Grkovic <157990654+dg235@users.noreply.github.com>
2024-03-06 11:22:34 +0200

    Update foo.py

--- foo.py
+++ foo.py
@@ -2,19 +2,20 @@ def is_divisible_by_k(x, k):
  '''
  Checks whether x is divisible by k.
  '''
- assert x%k == 0
+ return x % k == 0
 '''
 Store all the integers that are multiples of 2 or 5 or 7 that are lower 
 or equal to 1000 (excluding doubles)
 '''
-x = ()
-for i in range(1000):
- if (is_divisible_by_k(x, 2) & is_divisible_by_k(x, 3)) |
-is_divisible_by_k(x, 7):
+x = []
+for i in range(0, 1000):
+ if (is_divisible_by_k(i, 2) OR is_divisible_by_k(i, 3)) OR
+is_divisible_by_k(i, 7):
  x.append(i)
  
 '''
 Sum all the integers that are multiples of 2 or 5 or 7 that are lower 
 or equal to 1000 (excluding doubles)
 '''
-sum(x)
+sum = sum(x)
+print(sum)
